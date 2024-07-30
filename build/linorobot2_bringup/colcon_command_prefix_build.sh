191713095ed2f89369194f2f78a763137026a69b
carolina.campagna12 <carolina.campagna12@gmail.com>
2024-07-30 00:20:36 -0400

    Initial commit with submodules and other changes

---
+++ build/linorobot2_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ubuntu/linorobot2_ws/install/micro_ros_msgs/share/micro_ros_msgs/package.sh"
+. "/home/ubuntu/linorobot2_ws/install/micro_ros_agent/share/micro_ros_agent/package.sh"
